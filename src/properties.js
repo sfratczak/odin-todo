807540f3625f09a46965ca8475c9203a5be3b782
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2023-08-08 21:26:50 +0200

    Refactor properties to objects

---
+++ src/properties.js
@@ -0,0 +1,14 @@
+export const priority = {
+  critical: { priorityName: "Critical", priorityLevel: 0 },
+  high: { priorityName: "High", priorityLevel: 1 },
+  medium: { priorityName: "Medium", priorityLevel: 2 },
+  low: { priorityName: "Low", priorityLevel: 3 },
+};
+
+export const deadlineType = {
+  dueDateSet: { deadlineTypeName: "Due Date" },
+  anytime: { deadlineTypeName: "Anytime" },
+  sometime: { deadlineTypeName: "Sometime" },
+};
+
+export const DueDate = (() => {})();
