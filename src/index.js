390de6729810e58139617e13d2825855200d395e
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2023-08-30 14:17:49 +0200

    Refactor navbar generation to static code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
 import "./css/reset.css";
 import "./css/style.css";
-import { navBarItems } from "./properties";
-import { navBarModule } from "./domManager";
-
-navBarModule.renderNav(navBarItems);
